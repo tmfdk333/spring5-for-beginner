6809943ae36b42b222be22f498ed36a4b3837eeb
tmfdk333 <tmfdk333@gmail.com>
2021-04-22 22:19:28 +0900

    Chapter02 띄어쓰기 수정

--- chap02/src/main/java/basic/AppContext.java
+++ chap02/src/main/java/basic/AppContext.java
@@ -6,14 +6,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppContext {
     @Bean
-    public  Greeter greeter() {
+    public Greeter greeter() {
         Greeter g = new Greeter();
         g.setFormat("%s, 안녕하세요!");
         return g;
     }
 
     @Bean
-    public  Greeter greeter1() {
+    public Greeter greeter1() {
         Greeter g = new Greeter();
         g.setFormat("안녕하세요, %s님!");
         return g;
